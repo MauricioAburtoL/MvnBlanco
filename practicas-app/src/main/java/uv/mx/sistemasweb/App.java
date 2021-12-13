2948f980d41d9e68bdd019ede7edd958516b04b8
MauricioAburtoL <85853037+MauricioAburtoL@users.noreply.github.com>
2021-12-13 13:51:19 -0600

    Impresión

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+options("/", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", ""));
+
+        get("/aprobado", (rq, rs) -> {
+            System.out.println("Hola");
+            return "<h3>Ya pongame un 9 jeje</h3>";
+        });
     }
 }
